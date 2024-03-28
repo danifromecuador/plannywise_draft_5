777dbce8bf6c77fd784a2c037c70a9d8fa256f65
dani <danifromecuador@gmail.com>
2024-03-28 09:41:17 -0500

    Create a repetition alarm with several interval times

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -1,18 +1,66 @@
 // src/alarm/Alarm.jsx
 
+import { useState, useEffect } from 'react'
 import { useStore } from '../zustand/store'
 
 export const Alarm = () => {
-  const { currentDate } = useStore()
+  const { currentDate, updateCurrentDate } = useStore()
+  let [message, setMessage] = useState("")
+  let [minutesInterval, setMinutesInterval] = useState(15)
+  let [nextAlarmMessage, setNextAlarmMessage] = useState("")
 
-  const updateTime = () => {
-    console.log(currentDate);
+  const alarmWillSoundEachMinutes = (minutesInterval) => {
+    console.log(currentDate.second);
+    if (currentDate.second % minutesInterval == 0) setMessage("alarm is playing")
+    else setMessage("wait")
   }
+
+  const nextAlarmWillSoundAt = () => {
+    const remainder = currentDate.second % minutesInterval;
+    const nextMinute = currentDate.second + (minutesInterval - remainder);
+    const nextAlarmMinute = nextMinute >= 60 ? 0 : nextMinute;
+    console.log(remainder, nextMinute, nextAlarmMinute);
+
+    setNextAlarmMessage(nextAlarmMinute.toString().padStart(2, '0'));
+  }
+  // update date and time (currentDate) each second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      updateCurrentDate()
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // update next alarm will sound at message each time currentDate changes (each second)
+  useEffect(() => {
+    alarmWillSoundEachMinutes(minutesInterval)
+    nextAlarmWillSoundAt()
+  }, [currentDate])
 
   return (
     <div className="alarm">
-      local hour is {currentDate.hour} : {currentDate.minute} : {currentDate.second}
-      <button onClick={updateTime} >Update Time</button>
+      <div className="alarm-local-hour">
+        {currentDate.hour < 10 ? `0${currentDate.hour}` : currentDate.hour} : {currentDate.minute < 10 ? `0${currentDate.minute}` : currentDate.minute} : {currentDate.second < 10 ? `0${currentDate.second}` : currentDate.second}
+      </div>
+      <div className="alarm-setted-interval-indicator">
+        The alarm will sound every {minutesInterval} minutes
+      </div>
+      <div className="alarm-next-time-indicator">
+        Next alarm will sound at {currentDate.hour} : {currentDate.minute} : {nextAlarmMessage}
+      </div>
+      <div className="alarm-config">
+        <details>
+          <summary>Change interval</summary>
+          <div onClick={() => setMinutesInterval(5)}>5</div>
+          <div onClick={() => setMinutesInterval(10)}>10</div>
+          <div onClick={() => setMinutesInterval(15)}>15</div>
+          <div onClick={() => setMinutesInterval(20)}>20</div>
+          <div onClick={() => setMinutesInterval(30)}>30</div>
+          <div onClick={() => setMinutesInterval(60)}>60</div>
+        </details>
+      </div>
+      <div>{message}</div>
     </div>
   )
 }
