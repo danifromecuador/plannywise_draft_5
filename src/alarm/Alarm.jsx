81a429ac8ad8f9f3e653581d2c940d58786a4834
dani <danifromecuador@gmail.com>
2024-03-28 11:32:18 -0500

    Shrink code by shrinking variables names and Document functions

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -2,67 +2,74 @@
 import { useState, useEffect } from 'react'
 import { useStore } from '../zustand/store'
 
+// time       is a variable type object from the store that shows the current local time
+// updateTime is a function from the store that updates the time (this function is called each second)
+//            other way the time will be static
+// h, m, s    are the keys of the time object that represent the hours, minutes, and seconds 
 export const Alarm = () => {
-  const { currentDate, updateCurrentDate } = useStore()
-  let [currentDateFormatted, updateCurrentDateFormatted] = useState({ h: "", m: "", s: ""})
-  let [message, setMessage] = useState("")
+  const { time, updateTime } = useStore()
+  let [timeFormatted, setTimeFormatted] = useState({ h: time.h, m: time.m, s: time.s })
+  let [alarmStatusMessage, setAlarmStatusMessage] = useState("")
   let [minutesInterval, setMinutesInterval] = useState(15)
   let [nextAlarmMessage, setNextAlarmMessage] = useState("")
 
-  // format currentDate to transform from this: 9:7:0 to this: 09:07:00
-  const formatCurrentDate = () => {
-    currentDate.h < 10 ? currentDateFormatted.h = `0${currentDate.h}` : currentDateFormatted.h = currentDate.h;
-    currentDate.m < 10 ? currentDateFormatted.m = `0${currentDate.m}` : currentDateFormatted.m = currentDate.m;
-    currentDate.s < 10 ? currentDateFormatted.s = `0${currentDate.s}` : currentDateFormatted.s = currentDate.s;
+  // format time to transform from 9:17:0 to 09:17:00 only for visual purposes
+  const formatTime = () => {
+    setTimeFormatted({
+      h: time.h.toString().padStart(2, '0'),
+      m: time.m.toString().padStart(2, '0'),
+      s: time.s.toString().padStart(2, '0')
+    })
   }
 
-  // 
-  const alarmWillSoundEachMinutes = (minutesInterval) => {
-    if (currentDate.s % minutesInterval == 0) setMessage("alarm is playing")
-    else setMessage("wait")
+  const alarmWillSoundEach = () => {
+    if (time.s % minutesInterval == 0) setAlarmStatusMessage("alarm is playing")
+    else setAlarmStatusMessage("wait")
   }
 
+  // TODO check if it is working with 60 minutes
   const nextAlarmWillSoundAt = () => {
-    const remainder = currentDate.s % minutesInterval;
-    const nextMinute = currentDate.s + (minutesInterval - remainder);
+    const remainder = time.s % minutesInterval;
+    const nextMinute = time.s + (minutesInterval - remainder);
     const nextAlarmMinute = nextMinute >= 60 ? 0 : nextMinute;
 
     setNextAlarmMessage(nextAlarmMinute.toString().padStart(2, '0'));
   }
-  // update date and time (currentDate) each second
+
+  // update time each second, other way the time will be static
   useEffect(() => {
     const intervalId = setInterval(() => {
-      updateCurrentDate()
+      updateTime()
     }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  // update next alarm will sound at message each time currentDate changes (each second)
+  // run these functions each time that `time` is updated
   useEffect(() => {
-    alarmWillSoundEachMinutes(minutesInterval)
+    alarmWillSoundEach()
     nextAlarmWillSoundAt()
-    formatCurrentDate()
-  }, [currentDate])
+    formatTime()
+  }, [time])
 
   return (
     <div className="alarm">
-      <div className="alarm-local-hour">
-        {currentDateFormatted.h} : {currentDateFormatted.m} : {currentDateFormatted.s}
+      <div className="alarm-h-m-s">
+        {timeFormatted.h} : {timeFormatted.m} : {timeFormatted.s}
       </div>
       <div className="alarm-setted-interval-indicator">
-        The alarm will sound every {minutesInterval} minutes
+        The alarm will sound each {minutesInterval} minutes
       </div>
       <div className="alarm-next-time-indicator">
-        Next alarm will sound at {currentDate.h} : {currentDate.m} : {nextAlarmMessage}
+        Next alarm will sound at {time.h} : {time.m} : {nextAlarmMessage}
       </div>
       <div className="alarm-config">
         <details>
-          <summary>Change interval</summary>
+          <summary>Change minutes interval</summary>
           {[5, 10, 15, 20, 30, 60].map(i => <div key={i} onClick={() => setMinutesInterval(i)}>{i}</div>)}
         </details>
       </div>
-      <div>{message}</div>
+      <div>{alarmStatusMessage}</div>
     </div>
   )
 }
