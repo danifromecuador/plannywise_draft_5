e8ed1dea94d156903c80be5764edff1978dc376a
dani <danifromecuador@gmail.com>
2024-03-29 19:08:16 -0500

    Shrink code using DRY and KISS

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -1,6 +1,6 @@
 // src/alarm/Alarm.jsx
 import { useState, useEffect } from 'react'
-import { useStore } from '../zustand/store'
+import { ztore } from '../zustand/ztore'
 import './Alarm.css'
 
 // time       is a variable type object from the store that shows the current local time
@@ -8,14 +8,21 @@ import './Alarm.css'
 //            other way the time will be static
 // h, m, s    are the keys of the time object that represent the hours, minutes, and seconds 
 export const Alarm = () => {
-  const { time, updateTime, alarmInterval, updateAlarmInterval } = useStore()
+  const { time, updateTime, alarmInterval, updateAlarmInterval } = ztore()
   let [timeFormatted, setTimeFormatted] = useState({ h: time.h, m: time.m, s: time.s })
   let [alarmStatusMessage, setAlarmStatusMessage] = useState("")
-  let [minutesInterval, setMinutesInterval] = useState(alarmInterval)
   let [nextAlarmMessage, setNextAlarmMessage] = useState("")
-  console.log(alarmInterval);
 
-  // format time to transform from 9:17:0 to 09:17:00 only for visual purposes
+  // update ztore time each second, other way the time will be static
+  useEffect(() => {
+    const intervalId = setInterval(() => { updateTime() }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // format ztore time to transform from 9:17:0 to 09:17:00 only for visual purposes
+  // this change will not affect ztore time, because this change will be setted in
+  // timeFormatted, so this way, ztore time can be used to math operations
   const formatTime = () => {
     setTimeFormatted({
       h: time.h.toString().padStart(2, '0'),
@@ -24,41 +31,24 @@ export const Alarm = () => {
     })
   }
 
-  const alarmWillSoundEach = () => {
-    if (time.s % minutesInterval == 0) setAlarmStatusMessage("playing")
-    else setAlarmStatusMessage("wait")
-  }
+  const alarmWillSoundEach = () => time.s % alarmInterval == 0 ? setAlarmStatusMessage("playing") : setAlarmStatusMessage("wait")
+  
 
   // TODO check if it is working with 60 minutes
+  // calculate newt time that the alarm will sound
   const nextAlarmWillSoundAt = () => {
-    const remainder = time.s % minutesInterval;
-    const nextMinute = time.s + (minutesInterval - remainder);
+    const remainder = time.s % alarmInterval;
+    const nextMinute = time.s + (alarmInterval - remainder);
     const nextAlarmMinute = nextMinute >= 60 ? 0 : nextMinute;
 
     setNextAlarmMessage(nextAlarmMinute.toString().padStart(2, '0'));
   }
 
-  // update time each second, other way the time will be static
+  // run these functions each time that ztore time is updated (each second)
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      updateTime()
-      updateAlarmInterval(minutesInterval)
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  // update variable alarmInterval from the Store 
-  const handleChangeMinutesIntervalBtn = (i) => {
-    setMinutesInterval(i)
-    updateAlarmInterval(i)
-  }
-
-  // run these functions each time that `time` is updated
-  useEffect(() => {
+    formatTime()
     alarmWillSoundEach()
     nextAlarmWillSoundAt()
-    formatTime()
   }, [time])
 
   return (
@@ -69,12 +59,12 @@ export const Alarm = () => {
       </div>
       <div className="alarm-setted-interval-indicator">
         <span>The alarm will sound</span>
-        <span>each {minutesInterval} minutes</span>
+        <span>each {alarmInterval} minutes</span>
       </div>
       <div className="alarm-config">
         <details>
           <summary>Change interval</summary>
-          {[5, 10, 15, 20, 30, 60].map(i => <div key={i} onClick={() => handleChangeMinutesIntervalBtn(i)}>{i}</div>)}
+          {[5, 10, 15, 20, 30, 60].map(i => <div key={i} onClick={() => updateAlarmInterval(i)}>{i}</div>)}
         </details>
       </div>
       <div className="alarm-next-time-indicator">
