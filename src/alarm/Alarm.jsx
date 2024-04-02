734b1b5383f51a5cfaaf887fd294210c28400c1d
dani <danifromecuador@gmail.com>
2024-04-01 23:16:03 -0500

    Improve UI/UX

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -1,5 +1,6 @@
 import { ztore } from '../zustand/ztore.js';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import './Alarm.css'
 
 export const Alarm = () => {
   const { timeFormatted, currentAlarmInterval, updateCurrentAlarmInterval, updateZtore } = ztore();
@@ -11,22 +12,47 @@ export const Alarm = () => {
   }, [currentAlarmInterval]);
 
   // save ztore state in local storage when ztore state changes
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("currentAlarmInterval", JSON.stringify(currentAlarmInterval))
-  },[currentAlarmInterval]) 
+  }, [currentAlarmInterval])
+
+  const [hideAlarmIntervalShow, setHideAlarmIntervalShow] = useState("")
+  const [hideAlarmIntervalSelect, setHideAlarmIntervalSelect] = useState("hide")
+  const handleClickAlarmIntervalShow = () => {
+    if (hideAlarmIntervalShow === "") {
+      setHideAlarmIntervalShow("hide")
+      setHideAlarmIntervalSelect("")
+    }
+    else setHideAlarmIntervalShow("")
+  }
+
+  const handleClickAlarmIntervalSelect = (i) => {
+    setHideAlarmIntervalSelect("hide")
+    setHideAlarmIntervalShow("")
+    updateCurrentAlarmInterval(i)
+  }
+
+
 
   return (
     <div className="alarm">
       <div className="alarm-left">
-        <span>Current Local Time </span>
+        <span>Current Local Time</span>
         <span>{timeFormatted.h} : {timeFormatted.m} : {timeFormatted.s}</span>
       </div>
       <div className="alarm-center">
-        <span>The alarm will sound </span>
-        <span>
-          every...
-          <span className="alarm-interval-show" onClick={() => updateCurrentAlarmInterval(23)}>{currentAlarmInterval}</span>
-          ...minutes</span>
+        <span>The alarm will sound every</span>
+        <span className='puto'>
+          <span className={`alarm-interval-show ${hideAlarmIntervalShow}`} onClick={handleClickAlarmIntervalShow}>{currentAlarmInterval}</span>
+          <span className={`alarm-interval-select ${hideAlarmIntervalSelect}`}>
+            {[5, 10, 15, 20, 30, 60].map(i => <span key={i} className='alarm-interval-select-item' onClick={() => handleClickAlarmIntervalSelect(i)}>{i}</span>)}
+          </span>
+          minutes
+        </span>
+      </div>
+      <div className="alarm-right">
+        <span>Next alarm will sound at</span>
+        <span>11 : 11 : 00</span>
       </div>
     </div>
   );
