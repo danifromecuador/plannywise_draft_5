69fc1b2497ec483d206f063087282c63e536af57
dani <danifromecuador@gmail.com>
2024-04-01 22:17:27 -0500

    Save currentAlarmInterval in local storage

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -2,33 +2,31 @@ import { ztore } from '../zustand/ztore.js';
 import { useEffect } from 'react';
 
 export const Alarm = () => {
-  const { time, timeFormatted, alarmInterval, updateAlarmInterval, updateZtore } = ztore();
+  const { timeFormatted, currentAlarmInterval, updateCurrentAlarmInterval, updateZtore } = ztore();
 
+  // refresh ztore every second
   useEffect(() => {
     const intervalId = setInterval(updateZtore, 1000);
-    return () => clearInterval(intervalId);    
-  }, []);
+    return () => clearInterval(intervalId);
+  }, [currentAlarmInterval]);
 
-  const store = ztore()
+  // save ztore state in local storage when ztore state changes
   useEffect(()=>{
-    console.log(store);
-  },[store])
+    localStorage.setItem("currentAlarmInterval", JSON.stringify(currentAlarmInterval))
+  },[currentAlarmInterval]) 
 
   return (
     <div className="alarm">
       <div className="alarm-left">
-        <span>Current Local Time</span>
+        <span>Current Local Time </span>
         <span>{timeFormatted.h} : {timeFormatted.m} : {timeFormatted.s}</span>
       </div>
       <div className="alarm-center">
-        <span>Alarm will sound</span>
+        <span>The alarm will sound </span>
         <span>
-          each
-          <button onClick={()=>updateAlarmInterval(23)}>{alarmInterval.current}</button>
-          minutes
-        </span>
-      </div>
-      <div className="alarm-right">
+          every...
+          <span className="alarm-interval-show" onClick={() => updateCurrentAlarmInterval(23)}>{currentAlarmInterval}</span>
+          ...minutes</span>
       </div>
     </div>
   );
