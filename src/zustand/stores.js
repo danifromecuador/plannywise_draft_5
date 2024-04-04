38daf4e8853fe893d80bc5361a279a7ffd082f6d
dani <danifromecuador@gmail.com>
2024-04-04 18:22:47 -0500

    Create store for todo stuff

--- src/zustand/stores.js
+++ src/zustand/stores.js
@@ -1,6 +1,7 @@
 // src/zustand/stores.js
 import { create } from 'zustand';
 import { getTime, formatTime, previousAlarmInterval, nextAlarm, updateAlarmStore } from '../alarm/alarm_logic.js'
+import { todos, dones, addTodo, markAsDone } from '../todo/todo_logic.js'
 
 export const alarmStore = create((set) => ({
   time: getTime(),
@@ -10,4 +11,17 @@ export const alarmStore = create((set) => ({
   nextAlarm: nextAlarm(),
   updateCurrentAlarmInterval: (newCurrentAlarmInterval) => set({ currentAlarmInterval: newCurrentAlarmInterval }),
   updateAlarmStore: () => set(updateAlarmStore())
+}))
+
+
+
+export const todoStore = create((set) => ({
+  todos: todos,
+  dones: dones,
+  addTodo: (input) => addTodo(input),
+  markAsDone: () => markAsDone(),
+  updateTodoStore: () => {
+    set(todos)
+    set(dones)
+  }
 }))
