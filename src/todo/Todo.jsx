a131edea15e7fba0e43c1c5e28d78c20f87a7487
dani <danifromecuador@gmail.com>
2024-04-04 18:48:29 -0500

    Add a new goal to todos list

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,40 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { todoStore } from '../zustand/stores.js'
+import './Todo.css'
 
 export const Todo = () => {
   const { todos, dones, addTodo, markAsDone, updateTodoStore } = todoStore()
   const [input, setInput] = useState("")
+
   const handleInputChange = (e) => setInput(e.target.value)
 
   const handleEnterKeyDown = (k) => {
-    if (k.key === "Enter" && input !== "") {
+    if (k.key === "Enter" && input[0] !== " ") {
       addTodo(input)
-      updateTodoStore()
       setInput("")
-    }
+    }    
   }
-
-  useEffect(() => {
-
-  }, [])
-
-
 
   return (
     <div className="todo">
       <h1>Today's Goals</h1>
       <ul>
         {todos.map((e, i) => (
-          <li key={i}>{e.text}</li>
+          <li key={i} className='to-do'>{e.text}</li>
         ))}
         {dones.map((e, i) => (
-          <li key={i}>{e.text}</li>
+          <li key={i} className='done'>{e.text}</li>
         ))}
       </ul>
       <div>
         <input
           type="text"
-          placeholder='Type a new goal and press enter'
+          placeholder="Type a goal and press Enter"
           value={input}
           onChange={(e) => handleInputChange(e)}
           onKeyDown={(k) => handleEnterKeyDown(k)}
