3a66048f1983c2b57fd320ad5d74e4b75ce40e95
dani <danifromecuador@gmail.com>
2024-04-05 13:58:23 -0500

    Get date to use it as a unique index

--- src/todo/todo_logic.js
+++ src/todo/todo_logic.js
@@ -21,8 +21,32 @@ export const dones = [
   }
 ]
 
+const getDate = () => {
+  const date = new Date()
+  const Y = date.getFullYear()
+  const M = date.getMonth()+1
+  const D = date.getDate()
+  const h = date.getHours()
+  const m = date.getMinutes()
+  const s = date.getSeconds()
+  const ms = date.getMilliseconds()
+  const index = [Y, M, D, h, m, s, ms]
+  let newIndex = ""
+  for (let i = 0; i< index.length; i++) {
+    newIndex += index[i].toString()
+  }
+  return newIndex
+}
+
+export const updateTodosLocalStorage = (todos) => {
+  console.log("This is the data that will be saved in local storage");
+  console.log(todos);
+  console.log(getDate());
+}
+
 export const addTodo = (input) => {
-  todos.push({ date: "12", text: input })
+  const date = new Date()
+  todos.push({ date: date, text: input })
 }
 
 export const markAsDone = () => {
