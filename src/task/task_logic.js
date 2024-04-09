19560e50a840cdb3b624e02fd803c69b5dee5298
dani <danifromecuador@gmail.com>
2024-04-09 18:46:03 -0500

    Avoid adding a task if the time interval hasnt changed

--- src/task/task_logic.js
+++ src/task/task_logic.js
@@ -1,11 +1,17 @@
 import { TaskStore } from "../zustand/stores"
 
+const taskWasCompleted = (interval) => {
+  let previousInterval = TaskStore.getState().tasks.length > 0 ? TaskStore.getState().tasks[TaskStore.getState().tasks.length - 1].interval : "none"
+  return previousInterval.toString() === interval.toString()
+}
+
 export const addTask = (set, interval, input) => {
-  // const taskStore = TaskStore()
-  set(() => ({
-    tasks: [...TaskStore.getState().tasks, {
-      interval: { minH: interval.minH, minM: interval.minM, maxH: interval.maxH, maxM: interval.maxM },
-      text: input
-    }]
-  }))
+  if (taskWasCompleted(interval) === false) {
+    set(() => ({
+      tasks: [...TaskStore.getState().tasks, {
+        interval: { minH: interval.minH, minM: interval.minM, maxH: interval.maxH, maxM: interval.maxM },
+        text: input
+      }]
+    }))
+  }
 }
